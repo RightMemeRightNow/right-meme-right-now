9a400972072ed5692131bf4e4c2c91327b50ed85
hyunwlee-dev <hwlee3928@gmail.com>
2023-10-21 21:06:03 +0900

    init: right-meme-right-now project

---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <>right meme right now</>;
+}
---
+++ src/styles/global.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: pretendard;
+    src: url("/fonts/pretendard-Black.woff") format("truetype");
+  }
+  html,
+  body {
+    background: red;
+    padding: 0;
+    margin: 0;
+    font-family: pretendard, 'Ubuntu', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-size: 16px;
+    color: var(--natural-color); 
+    background: var(--background-color); 
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    font-family: pretendard, 'Ubuntu', sans-serif;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
