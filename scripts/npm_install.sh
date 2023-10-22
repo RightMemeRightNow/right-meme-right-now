41abdb3104e6ac2612189ab54e0dd0f3687ee7cf
Sangyeop Kim <sangykimiv@gmail.com>
2023-10-22 14:31:51 +0900

    chore: update codedeploy setup
    1. updated start_app.sh
    2. updated npm_install.sh script file

--- scripts/npm_install.sh
+++ scripts/npm_install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "update dependencies test"
-echo "current dir: $(pwd)"
-
+cd /home/ubuntu/rmrn
 npm install
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,5 +2,6 @@
 
 echo "start app test"
 echo "current dir: $(pwd)"
-
+cd /home/ubuntu/rmrn
+echo "current dir: $(pwd)"
 npm run build && npm run start
