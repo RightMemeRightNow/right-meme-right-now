6120e4bb41bbfd2e259f0da9ac63d1a13ac0e17b
hyunwlee-dev <hwlee3928@gmail.com>
2023-10-22 20:30:33 +0900

    feat: DefaultHead 컴포넌트 추가 with OGTag

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,3 +7,10 @@ export const Step = Object.freeze({
   ASSET: 5,
   FINISH: 6,
 });
+
+export const Meta = Object.freeze({
+  title: '이때이짤',
+  description: '이때이짤에서 당신의 현재 상황에 맞는 짤을 추천해드려요.',
+  url: 'https://right-meme-right-now.site',
+  ogi: 'https://right-meme-right-now.site/_next/image?url=%2Fimages%2Fadmiration.png&w=640&q=75',
+});
