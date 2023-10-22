279381888028897f135c845e2681426f98969302
minsoo <als494@naver.com>
2023-10-22 23:34:09 +0900

    style: 버튼, 이미지 등 스타일 수정

--- src/componets/Finish/index.jsx
+++ src/componets/Finish/index.jsx
@@ -24,7 +24,7 @@ const Finish = ({ selectedTags, setStep }) => {
     <S.FlexWrapper>
       <S.Wrapper>
         <Image src={'/images/question.png'} width={300} height={44} alt={'지금 내게 필요한 짤은?'} />
-        <Image src={'/images/admiration.png'} width={400} height={300} alt={'결과이미지'} />
+        <Image src={'/images/admiration.png'} layout='responsive' width={400} height={300} alt={'결과이미지'} />
         <S.TagWrapper>
           {selectedTags.map(() => {
             //todo
@@ -33,16 +33,19 @@ const Finish = ({ selectedTags, setStep }) => {
           <S.Tag>태그1</S.Tag>
           <S.Tag>태그2</S.Tag>
           <S.Tag>태그3</S.Tag>
+          <S.Tag>태그4</S.Tag>
+          <S.Tag>태그5</S.Tag>
+          <S.Tag>태그6</S.Tag>
         </S.TagWrapper>
       </S.Wrapper>
       <S.Wrapper>
         <S.RowWrapper>
-          <Button theme='secondary' width='40%' ><Image src={'/images/download_icon.png'} width={25} height={25}/></Button>
-          <Button theme='secondary' width='60%' onClick={onClickFindSimilar}>비슷한 짤 찾기</Button>
+          <Button padding='0' height='40px' theme='secondary' width='40%' ><Image src={'/images/download_icon.png'} width={30} height={30}/></Button>
+          <Button padding='0' height='40px' theme='secondary' width='60%' onClick={onClickFindSimilar}>비슷한 짤 찾기</Button>
         </S.RowWrapper>
         <S.RowWrapper>
-          <Button onClick={onClickCopyUrl}>공유하기</Button>
-          <Button theme='secondary' onClick={onClickRetry}>다시 해보기</Button>
+          <Button padding='0' height='40px' onClick={onClickCopyUrl}>공유하기</Button>
+          <Button padding='0' height='40px' theme='secondary' onClick={onClickRetry}>다시 해보기</Button>
         </S.RowWrapper>
         <S.RowWrapper>
           <button onClick={onClickNotGood}>별로예요 - 직접 올리기</button>
