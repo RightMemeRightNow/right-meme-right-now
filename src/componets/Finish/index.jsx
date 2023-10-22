51004390dafeef2e25b9f3dc790e6e13616adb4e
seonghun <pch468@naver.com>
2023-10-22 22:48:15 +0900

    feat: 최종 짤 가져와서 상태에 저장, 렌더링하는 로직 추가

--- src/componets/Finish/index.jsx
+++ src/componets/Finish/index.jsx
@@ -1,34 +1,47 @@
 import * as S from './Finish.style';
-import Image from "next/image";
-import Button from "@/componets/Button";
+import Image from 'next/image';
+import Button from '@/componets/Button';
+import { useEffect, useState } from 'react';
 
 const Finish = ({ selectedTags, setStep }) => {
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    getFinalImage().then(res => setImageUrl(res.image));
+  }, []);
+
   const onClickNotGood = () => {
     // eslint-disable-next-line no-undef
     alert('준비중');
-  }
-  
+  };
+
   const onClickRetry = () => {
-    setStep(0)
-  }
+    setStep(0);
+  };
 
   const onClickFindSimilar = () => {
     //todo
-  }
+  };
 
   const onClickCopyUrl = () => {
     //todo
-  }
+  };
 
   return (
     <S.FlexWrapper>
       <S.Wrapper>
-        <Image src={'/images/question.png'} width={300} height={44} alt={'지금 내게 필요한 짤은?'} />
-        <Image src={'/images/admiration.png'} width={400} height={300} alt={'결과이미지'} />
+        <Image
+          src={'/images/question.png'}
+          width={300}
+          height={44}
+          alt={'지금 내게 필요한 짤은?'}
+        />
+
+        <Image src={imageUrl} width={400} height={300} alt={'결과이미지'} />
+
         <S.TagWrapper>
           {selectedTags.map(() => {
             //todo
-            
           })}
           <S.Tag>태그1</S.Tag>
           <S.Tag>태그2</S.Tag>
@@ -37,12 +50,18 @@ const Finish = ({ selectedTags, setStep }) => {
       </S.Wrapper>
       <S.Wrapper>
         <S.RowWrapper>
-          <Button theme='secondary' width='40%' ><Image src={'/images/download_icon.png'} width={25} height={25}/></Button>
-          <Button theme='secondary' width='60%' onClick={onClickFindSimilar}>비슷한 짤 찾기</Button>
+          <Button theme="secondary" width="40%">
+            <Image src={'/images/download_icon.png'} width={25} height={25} />
+          </Button>
+          <Button theme="secondary" width="60%" onClick={onClickFindSimilar}>
+            비슷한 짤 찾기
+          </Button>
         </S.RowWrapper>
         <S.RowWrapper>
           <Button onClick={onClickCopyUrl}>공유하기</Button>
-          <Button theme='secondary' onClick={onClickRetry}>다시 해보기</Button>
+          <Button theme="secondary" onClick={onClickRetry}>
+            다시 해보기
+          </Button>
         </S.RowWrapper>
         <S.RowWrapper>
           <button onClick={onClickNotGood}>별로예요 - 직접 올리기</button>
@@ -50,6 +69,10 @@ const Finish = ({ selectedTags, setStep }) => {
       </S.Wrapper>
     </S.FlexWrapper>
   );
+};
+
+function getFinalImage() {
+  return fetch('api/getFinalImage', { method: 'GET' }).then(res => res.json());
 }
 
 export default Finish;
