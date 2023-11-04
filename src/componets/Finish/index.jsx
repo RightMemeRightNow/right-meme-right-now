6c2b7263a2eb8b5d567e4e97c90019071abca0ee
seonghun <pch468@naver.com>
2023-11-04 14:37:05 +0900

    fix: 에러 없는 버전으로 되돌리기

--- src/componets/Finish/index.jsx
+++ src/componets/Finish/index.jsx
@@ -13,9 +13,7 @@ const Finish = ({ answers, setAnswers, setStep, fetchedResult, fetchZzal }) => {
 
   useEffect(() => {
     fetchZzal();
-
-    setImageUrl(`/images/zzal/${fetchedResult?.imageNames[0]}.png`);
-  }, [fetchedResult]);
+  }, []);
 
   const shareClickHandler = () => {
     if (typeof window === 'undefined') return;
